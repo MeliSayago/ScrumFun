f14b4549023eba35f36b86c361f3e689cf8d2b68
nicoSieiro <sieiro.nicolas@gmail.com>
2018-03-21 14:43:37 -0300

    createSesion

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './react/containers/App';
+import CreateSesionContainer from './react/containers/CreateSesionContainer'
 import store from './react/store';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route,Switch } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/createsesion" component={CreateSesionContainer} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
---
+++ src/react/containers/CreateSesionContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CreateSesion from '../components/CreateSesion'
+
+
+export default class CreateSesionContainer extends React.Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return (
+            <CreateSesion/>
+        )
+    }
+}
