bd5f192c2304b49cbf9a34a5b2aea7eb2f77dd38
MeliSayago <melisayago@gmail.com>
2018-03-21 17:55:40 -0300

    /home y setup app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './react/containers/App';
-import CreateSesionContainer from './react/containers/CreateSesionContainer'
+import CreateSesionContainer from './react/containers/CreateSesionContainer';
 import store from './react/store';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route,Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
