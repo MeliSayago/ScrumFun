bd3c2c5dbc606c977c6b24b02315043b91126923
MeliSayago <33676074+MeliSayago@users.noreply.github.com>
2018-03-21 18:06:21 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './react/containers/App';
 import CreateSesionContainer from './react/containers/CreateSesionContainer'
