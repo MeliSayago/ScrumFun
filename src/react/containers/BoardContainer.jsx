56282a9d6d03dd017f6479301b3794951d9a4b11
Giovanny <eddy.rivera.echegaray@gmail.com>
2018-03-27 13:44:05 -0300

    Carousel Board

---
+++ src/react/containers/BoardContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cards from '../components/Cards'
+
+class BoardContainer extends React.Component {
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+
+}
+
+export default BoardContainer
