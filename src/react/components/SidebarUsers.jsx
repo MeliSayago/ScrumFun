c3b346d9bbb733c7d06392b835c30e0e4aeea728
MeliSayago <melisayago@gmail.com>
2018-04-12 20:00:38 -0300

    background theme

--- src/react/components/SidebarUsers.jsx
+++ src/react/components/SidebarUsers.jsx
@@ -8,7 +8,7 @@ export default ({ users, currentUrl, scrumMasters }) => (
     <h2>Users</h2>
     {users.map((user, index) => (
       <Badge className={user.card ? 'backgroundGreen' : 'nothing'} key={index}>
-       <h5>{user.name}</h5> 
+        <h5>{user.name}</h5>
       </Badge>
     ))}
     <br />
--- src/react/components/StoryResults.jsx
+++ src/react/components/StoryResults.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ButtonToolbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import '../components/SidebarUsers.css';
+
 export default ({
   cardModa,
   story,
@@ -10,34 +12,43 @@ export default ({
   active,
   uid,
   scrumMaster,
-  resultList
+  resultList,
 }) =>
   cardModa ? (
     <div>
-      <h1 className="text-center">Story name: {story.storyName}</h1>
+      <h1 className="text-center ">
+        <span>Story name: {story.storyName}</span>
+      </h1>
       <div>
-      <ul className="xop-grid">
-        {resultList && resultList.map((user, index) => (
-          cardModa.map(card => {
-            return card == user.points ? 
-            <li key={index}>
-              <button className="xop-box xop-img">
-                <img src={user.img} alt="" onClick={() => handleClickButton(card)}/>
-                <div>
-                  <h3>{user.size}</h3>
-                </div>
-              </button>
-            </li>
-            :""
-          })
-        ))}
-      </ul>
-      </div >
+        <ul className="xop-grid">
+          {resultList &&
+            resultList.map((user, index) =>
+              cardModa.map(card => {
+                return card == user.points ? (
+                  <li key={index}>
+                    <button className="xop-box xop-img">
+                      <img
+                        src={user.img}
+                        alt=""
+                        onClick={() => handleClickButton(card)}
+                      />
+                      <div>
+                        <h3>{user.size}</h3>
+                      </div>
+                    </button>
+                  </li>
+                ) : (
+                  ''
+                );
+              }),
+            )}
+        </ul>
+      </div>
       {scrumMaster[0] && scrumMaster[0].id === uid ? (
         <div className="finishButton">
           <ButtonToolbar>
             <Button
-            className="finishButton"
+              className="finishButton"
               name="nextStory"
               id="nextStory"
               onClick={() => nextStory(story.id)}
@@ -46,13 +57,14 @@ export default ({
               Next Story
             </Button>
             <Button
-            className="finishButton"
-            name="voteAgain" 
-            onClick={() => voteAgain()}>
+              className="finishButton"
+              name="voteAgain"
+              onClick={() => voteAgain()}
+            >
               Vote Again
             </Button>
           </ButtonToolbar>
-         </div>
+        </div>
       ) : (
         <div />
       )}
